a49b77921cfe966a986904519527c71684c49870
Nato <120518462+dsnato@users.noreply.github.com>
2025-04-08 09:52:53 -0300

    Update anotações_aleatórias.py

--- anotações_aleatórias.py
+++ anotações_aleatórias.py
@@ -1,11 +1,1 @@
-def mdc(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-# Testando o algoritmo
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-
-resultado = mdc(num1, num2)
-print(f"\n✅ O Máximo Divisor Comum entre {num1} e {num2} é {resultado}.")
+#
