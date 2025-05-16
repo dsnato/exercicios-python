25526163760d35c88a7496744abb6fd27a3df6b6
Nato <120518462+dsnato@users.noreply.github.com>
2025-05-16 16:33:44 -0300

    Update anotações_aleatórias.py

--- anotações_aleatórias.py
+++ anotações_aleatórias.py
@@ -1,1 +1,2 @@
-Segundou com muitos exercícios do livro de Introdução a Programação com Python! Essa semana termino esse livro.
+Trabalho de Cícero valendo 1 ponto entregue hoje.
+Estudos e implementação sobre o projeto 2. Travei na ULA.
