bd48bb9fbf2eb80e43bf85fe2d3385991a3ff615
Nato <120518462+dsnato@users.noreply.github.com>
2025-08-17 20:16:09 -0300

    Update algoritmo_de_euclides.py

--- algoritmo_de_euclides.py
+++ algoritmo_de_euclides.py
@@ -1,3 +1,4 @@
+# Calcula MDC
 def mdc(a, b):
     while b != 0:
         a, b = b, a % b
@@ -9,3 +10,4 @@ num2 = int(input("Digite o segundo número: "))
 
 resultado = mdc(num1, num2)
 print(f"\n✅ O Máximo Divisor Comum entre {num1} e {num2} é {resultado}.")
+
